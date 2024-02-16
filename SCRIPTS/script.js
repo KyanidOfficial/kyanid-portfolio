349cd0b9da39c07bdc818a4cb1b78a10640b7e75
KyanidOfficial <151477260+KyanidOfficial@users.noreply.github.com>
2024-02-16 22:52:11 +0100

    Clean up + icon

---
+++ SCRIPTS/script.js
@@ -0,0 +1,19 @@
+
+/*Widget*/
+var CloseButton = document.getElementById("closebutton");
+
+CloseButton.addEventListener("click", function() {
+    var widget = document.getElementById("widget");
+    var body = document.getElementById("body");
+        widget.style.display = "none";
+        body.style.overflow = "visible";
+});
+
+var widgetMain = document.getElementById("widget");
+var openButton = document.getElementById("notdone");
+var body = document.getElementById("body");
+
+openButton.addEventListener("click", function() {
+    widgetMain.style.display = "grid";
+    body.style.overflow = "hidden";
+});
