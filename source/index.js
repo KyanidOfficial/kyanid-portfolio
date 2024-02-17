53bef6f22219a7b4e982fec5437b092385c107ff
KyanidOfficial <151477260+KyanidOfficial@users.noreply.github.com>
2024-02-17 01:33:15 +0100

    Web Translate Pre-Build

---
+++ source/index.js
@@ -0,0 +1,23 @@
+function translate(lng, tagAttr){
+    var translate = new Translate();
+    translate.init(tagAttr, lng);
+    translate.process();
+    if(lng == 'en'){
+      $("#enTranslator").css('color', '#f4623a');
+      $("#czTranslator").css('color', '#212529');
+    } 
+    if(lng == 'cz'){
+      $("#czTranslator").css('color', '#f4623a');
+      $("#enTranslator").css('color', '#212529');
+    }
+}
+$(document).ready(function(){
+  //This is id of HTML element (English) with attribute lng-tag
+  $("#enTranslator").click(function(){
+    translate('en', 'lng-tag');
+  });
+  //This is id of HTML element (Czech) with attribute lng-tag
+  $("#czTranslator").click(function(){
+    translate('cz', 'lng-tag');
+  });
+});
