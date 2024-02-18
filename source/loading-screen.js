8d4d8345fa3a90f27b3de30c0cdee7ee3ae968b1
KyanidOfficial <151477260+KyanidOfficial@users.noreply.github.com>
2024-02-18 14:49:48 +0100

    Loading Screen

---
+++ source/loading-screen.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".__loading_screen_box");
+    const body = document.querySelector("body");
+
+    loader.classList.add("__hidden");
+    body.style.overflowY = "scroll";
+    
+    setTimeout(1000)
+    
+    loader.remove(".__loading_screen_box");
+})
